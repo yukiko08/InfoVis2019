5f72efc602f9ef96667b7f44175a8f3557c3d919
吉川　裕木子 <199p214p@istc-a-029.epc.center.kobe-u.ac.jp>
2019-05-08 14:27:23 +0900

    w06

--- W06/main01.js
+++ W06/main01.js
@@ -22,11 +22,19 @@ function main()
     document.body.appendChild( renderer.domElement );
 
     var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
-    var material = new THREE.MeshLambertMaterial();
+    //var material = new THREE.MeshLambertMaterial();
+    var material = new THREE.ShaderMaterial({
+          vertexColors: THREE.VertexColors,
+          vertexShader: document.getElementById('gouraud.vert').text,
+          fragmentShader: document.getElementById('gouraud.frag').text,
+          uniforms: {
+          light_position: { type: 'v3', value: light.position }
+          }
+
+    });
 
     var torus_knot = new THREE.Mesh( geometry, material );
     scene.add( torus_knot );
-
     loop();
 
     function loop()
